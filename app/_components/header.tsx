87d8ad45886a9de65a1af256036630e308559490
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-11-08 15:49:41 -0300

    feat: add omponent initial Header

---
+++ app/_components/header.tsx
@@ -0,0 +1,21 @@
+import { ArrowUpRight } from "lucide-react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="bg-black px-4 py-2">
+      <div className="flex items-center justify-center w-full gap-3">
+        <span className="bg-[#F50056] px-[11px] py-1 rounded-3xl text-white text-xs">
+          14:57
+        </span>
+        <p className="text-white text-xs ">Por tempo limitado</p>
+        <Button className="text-white text-xs p-0 font-light" variant="link">
+          Garantir agora
+          <ArrowUpRight size={16} />
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
